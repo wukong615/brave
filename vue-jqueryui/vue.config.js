7ab5f2bceafe82710fac29fff7fa91ea1ee9dc18
chichangqing <chichangqing1987@163.com>
2019-04-10 10:02:26 +0800

    vue 使用 jQueryUI 的 Draggable/Droppable/Sortable

--- vue-jqueryui/vue.config.js
+++ vue-jqueryui/vue.config.js
@@ -30,18 +30,7 @@ module.exports = {
         https: false,
         hot: true,
         hotOnly: true,
-        open: false,
-        //配置自动启动浏览器
-        // proxy: {
-            // 配置多个代理(配置一个 proxy: 'http://localhost:4000' )
-            // '/api': {
-            //   target: 'http://192.168.1.248:9888',
-            //   // target: 'http://192.168.1.4:8999',
-            //   pathRewrite: {
-            //     '^/api': '/api'
-            //   }
-            // }
-        // }
+        open: false
     },
     pluginOptions: {
         // 第三方插件配置
